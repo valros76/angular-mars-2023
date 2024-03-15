7a95d9c6e312d7100d8d2c716a4861af386bb999
valros76 <valerian.dufrene@gmail.com>
2024-03-15 10:03:33 +0100

    Add first form exemple

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    FormsModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  userEmail:string = "test@mail.fr";
+
   constructor(private router:Router){}
+
+  onSubmitForm(){
+    console.log(this.userEmail);
+  }
 }
